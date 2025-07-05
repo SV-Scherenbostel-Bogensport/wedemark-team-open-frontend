d7ed6a89f892cf12aaf699c54b67f9ccb4206f35
Piet Ostendorp <79719028+Laubfrosch49@users.noreply.github.com>
2025-07-06 01:36:12 +0200

    Add TournamentNavbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script lang="ts" setup>
 import Navbar from '@/components/AppNavbar.vue'
+import TournamentNavbar from './components/TournamentNavbar.vue'
 // import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <Navbar />
+  <TournamentNavbar />
 </template>
---
+++ src/components/TournamentNavbar.vue
@@ -0,0 +1,44 @@
+<script lang="ts" setup>
+import { RouterLink, useRoute } from 'vue-router'
+// import logo from '@/assets/img/indi.png'
+
+const isActiveLink = (routePath: string) => {
+  const route = useRoute()
+  return route.path === routePath
+}
+
+const navItems = [
+  { label: 'Info', to: '/' },
+  { label: 'Zeitplan', to: '/schedule' },
+  { label: 'Teams', to: '/teams' },
+  { label: 'Platzierung', to: '/placement' },
+  { label: 'Ergebnisse', to: '/results' },
+  { label: 'Live', to: '/live', hidden: true },
+  { label: 'Fotos', to: '/pictures', hidden: true },
+]
+</script>
+
+<template>
+  <div class="bg-red-700 py-4">
+    <ul class="flex h-5 justify-center items-center">
+      <template v-for="(item, index) in navItems.filter((i) => !i.hidden)" :key="index">
+        <li>
+          <RouterLink
+            :to="item.to"
+            :class="[
+              isActiveLink(item.to) ? 'underline' : 'hover:underline',
+              'text-white font-extrabold px-4 py-2 rounded-md uppercase text-lg',
+            ]"
+          >
+            {{ item.label }}
+          </RouterLink>
+        </li>
+
+        <!-- Trennpunkt zwischen den Einträgen -->
+        <li v-if="index !== navItems.filter((i) => !i.hidden).length - 1" aria-hidden="true">
+          <span class="text-white select-none">•</span>
+        </li>
+      </template>
+    </ul>
+  </div>
+</template>
