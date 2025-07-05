674adfa76189c93bfd226bdd884fe8753c355ae0
Piet Ostendorp <79719028+Laubfrosch49@users.noreply.github.com>
2025-07-05 22:29:40 +0200

    Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,16 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
+  server: {
+    port: 3000,
+    host: true, // Allows access from other devices on the network
+    strictPort: true, // Ensures the server will not start if the port is already in use
+    open: true, // Opens the browser automatically
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
